e1af83f7cb2bffe78e84853f8ccd03c4c4efc2ce
asukhrin <asukhrin@kmilearning.com>
2019-01-04 03:37:37 +0200

    update urls

--- djtbot/tgbot/urls.py
+++ djtbot/tgbot/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import bot_view
+from .views import bot_view, start
 from django.conf import settings
-
+from django.urls import re_path
 
 urlpatterns = [
+    re_path(r'^$', start, name='start'),
     path('{}'.format(settings.TOKEN), bot_view, name='bot_view'),
 ]
