8a2ead6a6fdc62a82de233c71dd518738871af86
asukhrin <asukhrin@kmilearning.com>
2019-01-23 21:19:36 +0200

    fix celery settings

--- djtbot/djtbot/celeryapp.py
+++ djtbot/djtbot/celeryapp.py
@@ -19,6 +19,6 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 
-@app.task(bind=True, crontab(hour='*/4'))
+@app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
